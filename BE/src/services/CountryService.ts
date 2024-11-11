18a18503ebbbd98a593e67e57599f638634ef6c6
ronnymv <rjmv.dev@gmail.com>
2024-11-11 20:45:06 -0300

    Feat(BE): Implement service.

---
+++ BE/src/services/CountryService.ts
@@ -0,0 +1,28 @@
+
+import { ApiClient } from './../utils/apiClient';
+
+export class CountryService {
+  private static AVAILABLE_COUNTRIES_URL = 'https://date.nager.at/api/v3/AvailableCountries';
+  private static COUNTRY_INFO_URL = 'https://date.nager.at/api/v3/CountryInfo';
+  private static POPULATION_DATA_URL = 'https://countriesnow.space/api/v0.1/countries/population';
+  private static FLAG_URL = 'https://countriesnow.space/api/v0.1/countries/flag/images';
+
+  public async getAvailableCountries() {
+    return await ApiClient.get(CountryService.AVAILABLE_COUNTRIES_URL);
+  }
+
+  public async getCountryInfo(countryCode: string) {
+    const countryInfo = await ApiClient.get(`${CountryService.COUNTRY_INFO_URL}/${countryCode}`);
+    const populationData = await ApiClient.get(CountryService.POPULATION_DATA_URL);
+    const flagData = await ApiClient.get(CountryService.FLAG_URL);
+
+    const population = populationData.data.find((country: any) => country.iso3 === countryCode)?.populationCounts || [];
+    const flag = flagData.data.find((country: any) => country.iso3 === countryCode)?.flag || '';
+
+    return {
+      ...countryInfo,
+      population,
+      flag,
+    };
+  }
+}
