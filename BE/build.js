bc30534d9d3415ec0543d0f0f61f7b588876dc05
ronnymv <rjmv.dev@gmail.com>
2024-11-11 20:13:03 -0300

    Chore(BE): Install dependecies and back structure.

---
+++ BE/build.js
@@ -0,0 +1,8 @@
+// build.js
+require('esbuild').build({
+  entryPoints: ['src/server.ts'],
+  outfile: 'dist/server.js',
+  bundle: true,
+  platform: 'node',
+  target: 'node14',
+}).catch(() => process.exit(1));
