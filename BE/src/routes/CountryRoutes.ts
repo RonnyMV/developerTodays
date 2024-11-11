bb966068e1c215aa9ab40520009a101973d710c0
ronnymv <rjmv.dev@gmail.com>
2024-11-11 20:45:22 -0300

    Feat(BE): Implement routes.

---
+++ BE/src/routes/CountryRoutes.ts
@@ -0,0 +1,13 @@
+// src/routes/CountryRoutes.ts
+import { Router } from 'express';
+import { CountryController } from './../controllers/countryController';
+
+const router = Router();
+const countryController = new CountryController();
+
+router.get('/countries', countryController.getAll);
+router.get('/countries/:countryCode', countryController.getOne);
+
+
+
+export default router;
