f64c587d13a1f2fd7f3c664cd2fafcc2b4d2e240
ronnymv <rjmv.dev@gmail.com>
2024-11-12 01:33:18 -0300

    Fix(BE): Change to env variable.

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -13,7 +13,7 @@ export class App {
   private middlewares() {
 
     this.app.use((req: Request, res: Response, next: NextFunction) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+      res.header('Access-Control-Allow-Origin', process.env.FE_IP);
       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
       res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
       res.header('Access-Control-Allow-Credentials', 'true'); 
