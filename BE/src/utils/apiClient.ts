7e080fb537aa03bcc07cc42bd21c5c473fc54183
ronnymv <rjmv.dev@gmail.com>
2024-11-12 01:19:02 -0300

    Feat(BE): Implement http methods and env example.

--- BE/src/utils/apiClient.ts
+++ BE/src/utils/apiClient.ts
@@ -1,4 +1,3 @@
-// src/utils/ApiClient.ts
 import axios from 'axios';
 
 export class ApiClient {
@@ -6,6 +5,33 @@ export class ApiClient {
   public static async get(url: string) {
     try {
       const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error fetching data from ${url}: ${error}`);
+    }
+  }
+
+  public static async post(url: string) {
+    try {
+      const response = await axios.post(url);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error fetching data from ${url}: ${error}`);
+    }
+  }
+
+  public static async delete(url: string) {
+    try {
+      const response = await axios.delete(url);
+      return response.data;
+    } catch (error) {
+      throw new Error(`Error fetching data from ${url}: ${error}`);
+    }
+  }
+
+  public static async update(url: string) {
+    try {
+      const response = await axios.update(url);
       return response.data;
     } catch (error) {
       throw new Error(`Error fetching data from ${url}: ${error}`);
