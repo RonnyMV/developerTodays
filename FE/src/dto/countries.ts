c94c5c1d4ddb35803fe63f5e0927b8dd85a0143f
ronnymv <rjmv.dev@gmail.com>
2024-11-12 01:12:38 -0300

    Feat(FE): Implement DTOs.

---
+++ FE/src/dto/countries.ts
@@ -0,0 +1,37 @@
+export interface BorderCountry {
+  commonName: string;      
+  officialName: string;   
+  countryCode: string;    
+  region: string;        
+  borders: null | BorderCountry[]; 
+}
+
+export interface CountryDTO {
+  commonName: string; 
+  officialName: string;    
+  countryCode: string;    
+  region: string;
+  borders: BorderCountry[]; 
+  population: number[];
+  flag: string;
+}
+
+export interface CountryResumedDTO { 
+  countryCode: string;
+  name: string;
+}
+
+export interface Country {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: Border[];
+  flag: string;
+  population: number[];
+}
+
+export interface Border {
+  commonName: string;
+  countryCode: string;
+}
